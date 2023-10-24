53d64a9f2abc7d7261c4615fc142180d87a98a09
Qinruo <qinruoyang@outlook.com>
2023-10-24 13:19:28 -0700

    finish hw

--- app/src/main/java/edu/uw/ischool/qy54/biggobutton/MainActivity.kt
+++ app/src/main/java/edu/uw/ischool/qy54/biggobutton/MainActivity.kt
@@ -2,10 +2,29 @@ package edu.uw.ischool.qy54.biggobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+
+    private var pushCount = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pushButton: Button = findViewById(R.id.pushButton)
+        pushButton.setOnClickListener {
+            pushCount++
+            updateButtonText()
+        }
+    }
+
+    private fun updateButtonText() {
+        val pushButton: Button = findViewById(R.id.pushButton)
+        val text = when (pushCount) {
+            1 -> getString(R.string.pushed_once, pushCount)
+            else -> getString(R.string.pushed_multiple, pushCount)
+        }
+        pushButton.text = text
     }
 }
